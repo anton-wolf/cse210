d1762158173a109d5d3d1aa1afa7e12cb1e04ca7
Katlego <kat@geekpop.io>
2025-03-30 13:32:15 +0200

    Week03 activity

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,60 @@
+﻿namespace Fractions;
+
+public class Fraction
+{
+    // attributes
+    private int _top;
+    private int _bottom;
+
+    // constructors
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // accessors and mutators (getter and setter)
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    
+    // methods
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
