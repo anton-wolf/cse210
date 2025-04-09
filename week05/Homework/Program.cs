fa1655b1eda379aa5a878998be752647604ea937
Katlego <kat@geekpop.io>
2025-04-09 19:06:01 +0200

    Week5 learning activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,22 @@
-using System;
+namespace Homework;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment assign1 = new MathAssignment("Katlego Kgwetiane",
+            "Fractions",
+            "7.3",
+            "8-19");
+
+        WritingAssignment assign2 = new WritingAssignment("Kairo Kgwetiane",
+            "European History",
+            "The Causes of World War II");
+
+        Console.WriteLine(assign1.GetHomeWorkList());
+
+        Console.WriteLine();
+
+        Console.WriteLine(assign2.GetWritingInformation());
     }
 }
